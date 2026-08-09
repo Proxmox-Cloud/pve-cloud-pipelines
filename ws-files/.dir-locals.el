5f1ab9f06a2201a1452abd9c83124a7aea4a50bc
tobi <tobi_h@outlook.com>
2026-08-09 14:23:45 +0000

    remove emacs dirs

--- ws-files/.dir-locals.el
+++ ws-files/.dir-locals.el
@@ -3,11 +3,7 @@
 
 ((nil . ((python-venv-project . "~/.pve-cloud-dev-venv") ;; this autosources in vterm and sets pylsp jedi env
          ;; todo: custom shit attempts to fix lsp, maybe solved by just dumping pyright
-         (lsp-additional-ignore-directories . ("ansible_collections"
-                                               "/target\\'"
-                                               "/\\.terraform\\'"
-                                               "/\\.pytest_cache\\'"
-                                               "/dist\\'"))
+         ;; (lsp-additional-ignore-directories . ("/dist\\'"))
          (eval . (setq magit-repository-directories
                        (list (cons (locate-dominating-file default-directory ".dir-locals.el") 1)
                              (cons (expand-file-name "ansible_collections/pxc/cloud/" (locate-dominating-file default-directory ".dir-locals.el")) 0)))))))
